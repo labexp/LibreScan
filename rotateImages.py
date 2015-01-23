f0c1777a31e90fe99c66c401791b5d2c5c9da3a9
melalonso <melvin.aep@gmail.com>
2015-01-23 23:38:57 +0000

    Archivos modificados y agregados 5pm 23/01/15
    Se completo el script en bash enlazando todos los pasos para escanear y obtener un PDF. El script py rotateImages fue cambiado por uno mas sencillo, que verifica si es la imagen de la izq o derecha sin necesidad de metadata. El script rename se conserva igual. Se agrega una prueba hecha bookscan... con algunos problemas de luminosidad, pero se puede observar un pdf terminado.

--- rotateImages.py
+++ rotateImages.py
@@ -5,46 +5,28 @@ import os
 from wand.image import Image
 
 #Funcion reutilizada de autorotate.py de spreads para rotar las imagenes una vez tomadas con chdkptp.
-def autorotate_image(in_path, out_path):
-	try:
-		metadata = pyexiv2.ImageMetadata(in_path)
-		metadata.read()
-		
-		orient = int(metadata['Exif.Image.Orientation'].value)
-		print metadata
-		print orient
-	except:
-		return
+def autorotate_image(name, in_path, out_path):
+    num  = int(os.path.splitext(name)[0])
+    img = Image(filename=in_path)
 
-	img = Image(filename=in_path)
-	if orient == 1:
-		shutil.copyfile(in_path, out_path)
-		return
-	elif orient == 2:
-		img.flip()
-	elif orient == 3:
-		img.rotate(180)
-	elif orient == 4:
-		img.flop()
-	elif orient == 5:
-		img.rotate(90)
-		img.flip()
-	elif orient == 6:
-		img.rotate(90)
-	elif orient == 7:
-		img.rotate(270)
-		img.flip()
-	elif orient == 8:
-		img.rotate(270)
-	img.save(filename=out_path)
+    if(num%2 != 0):
+    	img.rotate(270)
+    else:
+    	img.rotate(90)
+    img.save(filename=out_path)
 
 
 #Rota todas las imagenes del directorio actual y las coloca en una carpeta creada llamada rotated.
-def rotateImages():
-    path = os.getcwd()
-    for fn in os.listdir(path):
-        if fn != 'rotateImages.py':
-            autorotate_image(fn,'rotated/'+os.path.splitext(fn)[0]+"_rotated.jpg")
+def rotateImages(folder):
+    path = os.getcwd()+"/"+folder+"/"+"renamed"
+    print("Rotating images...")
+    arr = os.listdir(path)
+    percentage = 100/len(arr)
+    progress = 0
+    for fn in arr:
+        autorotate_image(fn,path+"/"+fn,os.getcwd()+"/"+folder+"/"+"rotated/"+os.path.splitext(fn)[0]+"_rotated.jpg")
+        progress+=percentage
+        print(str(progress)+"%")
+    print("100% \nRotation successful!")
 
 
-rotateImages()
