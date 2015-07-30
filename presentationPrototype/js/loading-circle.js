2582348592f29b68b45697418d26071660ee5ff8
diugalde <dua.0695@gmail.com>
2015-07-29 22:14:20 -0500

    Adding missing pages:
    - capture.html (empty)
    - preview.html
    - Styles for new pages.
    - Making the project navigable

--- presentationPrototype/js/loading-circle.js
+++ presentationPrototype/js/loading-circle.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-	$('.demo').circleDiagram({
-		"percent": "65%",
-		"size": "320",
-		"borderWidth": "10",
-		"bgFill": "#D1C7AB",
-		"frFill": "#260126",
-		"textSize": "56",
-		"textColor": "#8C6954"
-	});
+
+
+
 });
