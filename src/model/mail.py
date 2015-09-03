988767e14c97d67b4232d4392566b0cb1538b705
diugalde <dua.0695@gmail.com>
2015-09-03 16:49:55 -0500

    Adding project and mail Popos

---
+++ src/model/mail.py
@@ -0,0 +1,10 @@
+__author__ = 'diugalde'
+
+
+class Mail:
+
+    def __init__(self, name, sender, phone, msg):
+        self.name = name
+        self.sender = sender
+        self.phone = phone
+        self.msg = msg
