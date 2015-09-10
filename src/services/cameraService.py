e684d449fbf7d9b6302ec5e9bb99ba486d295995
diugalde <dua.0695@gmail.com>
2015-09-10 18:21:50 -0500

    Working in CameraService and chdkptp
    - Adding Camera POPO.
    - Adding Chdkptp methods.
    - Adding multithreading shoots in camera service.

--- src/services/cameraService.py
+++ src/services/cameraService.py
@@ -1,18 +1,38 @@
 __author__ = 'tonyzeru'
 
+from model.camera import Camera
+from services.utils.chdkptp import Chdkptp
+import multiprocessing
+
+
 class CameraService:
 
-    def shoot(self):
-        pass
+    global savePath
+    savePath = "/home/diugalde/LibreScanProjects/L1/raw/"
 
-    def retake(self):
-        pass
+    picNumber = 0
 
-    def configure(self, pConfig):
-        pass
+    def __init__(self):
+        self.cams = [Camera("head"), Camera("tail")]
 
-    def prepare(self):
-        pass
+    def takePictures(self):
+        jobs = []
+        for cam in self.cams:
+            CameraService.picNumber += 1
+            process = multiprocessing.Process(target=Chdkptp.shoot, args=(cam, savePath+str(CameraService.picNumber)))
+            jobs.append(process)
+            process.start()
 
-    def remove(self):
-        pass
+        for j in jobs:
+            j.join()
+
+    def prepareCams(self):
+        for cam in self.cams:
+            Chdkptp.prepare(cam)
+        for cam in self.cams:
+            Chdkptp.recMode(cam)
+
+
+cs = CameraService()
+cs.prepareCams()
+cs.takePictures()
