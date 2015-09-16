8b06b2626a88332de027c01ef44e726221d23ee1
diugalde <dua.0695@gmail.com>
2015-09-15 20:10:16 -0500

    Working in the workflow service and taskmanager class
    - Iterating through task list working.
    - Generating output (only pdf for now)

--- src/services/utils/tesseract.py
+++ src/services/utils/tesseract.py
@@ -1,16 +1,16 @@
 import subprocess
-from subprocess import call
 from services.utils.task import Task
 
 
 class Tesseract(Task):
 
-    def __init__(self, p_language='spa', p_format='hocr'):
+    def __init__(self, p_language='spa', p_format='hocr', p_photo_extension='tif'):
         self.language = p_language
         self.output_format = p_format
+        self.photo_extension = p_photo_extension
 
     def exec(self, params):
-        input_path = params['input_dir'] + '/' + params['photo']
-        output_path = params['input_dir'] + '/' + params['photo'].split('.')[0]
+        input_path = params['input_dir'] + '/' + params['photo'] + "." + self.photo_extension
+        output_path = params['input_dir'] + '/' + params['photo']
         command = "tesseract -l " + self.language + " " + input_path + " " + output_path + " " + self.output_format
         subprocess.call(command, shell=True)
