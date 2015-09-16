8de19d83057219a9c2cf2ea76e940e04c95519cd
melalonso <melvin.aep@gmail.com>
2015-09-16 15:40:36 +0000

    Tesseract.py exec working

--- src/services/utils/tesseract.py
+++ src/services/utils/tesseract.py
@@ -5,7 +5,13 @@ from subprocess import call
 
 class Tesseract(Task):
 
-    def exec(self,p_language,p_format, p_input_dir):
-           call( "parallel tesseract -l "+ p_language + " {} {.} " + p_format+  " ::: " + p_input_dir,shell=True)
+    def __init__(self,p_language,p_format, p_input_dir):
+        self.language = p_language
+        self.format = p_format
+        self.input_dir = p_input_dir
 
+
+    def exec(self):
+           call( "parallel tesseract -l "+ self.language + " {} {.} " + self.format+  " ::: " + self.input_dir,shell=True)
+            
 
