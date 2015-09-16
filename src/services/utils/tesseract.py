adf46b8276212ff95c08160134c3267ce9b46eb1
melalonso <melvin.aep@gmail.com>
2015-09-16 14:29:56 +0000

    Finished tesseract.py

--- src/services/utils/tesseract.py
+++ src/services/utils/tesseract.py
@@ -1,9 +1,11 @@
 __author__ = 'melalonso'
 
 from task import Task
-
+from subprocess import call
 
 class Tesseract(Task):
 
-    def exec(self):
-        pass
+    def exec(self,p_language,p_format, p_input_dir):
+           call( "parallel tesseract -l "+ p_language + " {} {.} " + p_format+  " ::: " + p_input_dir,shell=True)
+
+
