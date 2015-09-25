e0cc14b6cf2604ed412e13871019b0376c2d2eae
5up3rD4n1 <superdaniel1920@hotmail.com>
2015-09-24 23:00:04 -0600

    Creating new service with queue
    	- Creating threads to process images
    	- New module to queue the complete process
    	- Adding serivice queueService(p_working_threads=3)

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from services.cameraService import CameraService
 from PIL import Image
 from services.utils.taskManager import TaskManager
-
-
+from services.queueService import queueSerivice
+import threading
+'''
 cs = CameraService()
 cs.prepare_cams()
 userInput = input("Presione una tecla para tomar una foto o s para salir: ")
@@ -13,4 +14,12 @@ while userInput != "s":
 # Procesa 6 imagenes, recordar cambiar la ruta en CameraService y en la línea siguiente.
 t = TaskManager("/home/diugalde/LibreScanProjects/L1/raw/")
 t.process(["1", "2", "3", "4", "5", "6"])
-t.generate()
+t.generate() '''
+def main():
+    q = queueSerivice()
+    q.push(["010", "012", "013", "014", "015", "016"])
+    print(q.get_active_threads())
+
+
+
+
--- src/services/queueService.py
+++ src/services/queueService.py
@@ -2,22 +2,35 @@ __author__ = 'sd'
 
 from queue import Queue
 import threading
+from services.utils.taskManager import TaskManager
 
 
-q = Queue()
+class queueSerivice:
 
-def worker():
-    while True:
-        item = q.get()
-        do_work(item)
-        q.task_done()
+    def start(self):
 
+        while True:
+            item = [self.queue.get(block=True)]
+            print("Procesando imagen: "+item[0])
+            t = TaskManager("/home/sd/Documents/prueba/pruebaSpreads/SD")
+            t.process(item)
+            self.queue.task_done()
 
-for i in range(num_worker_threads):
-     t = threading.Thread(target=worker)
-     t.daemon = True
-     t.start()
+    def push(self, p_image):
+        if isinstance(p_image, list):
+            for image in p_image:
+                #print(image)
+                self.queue.put(image)
+        else:
+            self.queue.put(p_image)
 
-for item in source():
-    q.put(item)
+    def __init__(self, p_worker_threads=2):
+        self.queue = Queue()
+        self.worker_threads = p_worker_threads
+        for i in range(self.worker_threads):
+            t = threading.Thread(target=self.start)
+            t.setDaemon(True)
+            t.start()
 
+    def get_active_threads(self):
+        return threading.active_count()
