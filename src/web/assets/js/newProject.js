62cade5e26b602b7318a6d06a4bdb11eca165e99
diugalde <dua.0695@gmail.com>
2016-02-04 00:11:03 -0600

    Prepare cams modal and delete-pics files working
    - Jade, css and js for prepare cams modal.
    - Create .pics.ls and .toDelete.ls on project creation.
    - Insert and delete to .pics.ls and .toDelete.ls
    - Prepare cams method working

--- src/web/assets/js/newProject.js
+++ src/web/assets/js/newProject.js
@@ -5,7 +5,7 @@ var ready = function(){
         var p_description = $('#newproject-description').val();
         var zoom = $('#configuration-zoom').val();
         var p_language = $('#newproject-language').val();
-        $("#loading-cams").css("display", "block");
+
         $.ajax({
             method: 'POST',
             url: '/project',
@@ -14,12 +14,11 @@ var ready = function(){
             data: JSON.stringify({postData: {project_name: p_name, project_description: p_description, config:{zoom:zoom, language:p_language}}})
         }).done(function(data) {
             if(data.status == 1){
-                window.location.href = '/scan';
-                $("#loading-cams").css("display", "none");
+                $('#prepare-cams-modal').modal('show');
+                $('#modal-pcm-created').css('display', 'block');
                 console.log("The project was successfully created.");
             }else{
-                $("#loading-cams").css("display", "none");
-                $('#camera-error').modal('show');
+                alert('Error creando estructura de proyecto');
             }
         });
     });
