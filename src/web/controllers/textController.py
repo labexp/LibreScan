adf5e2ede93f261a98b8e068cc0bea4b1dd8287c
Julio_R <j.rojas.c123@gmail.com>
2017-09-20 15:13:44 -0600

    Jabiru now is working with the hOCR library

--- src/web/controllers/textController.py
+++ src/web/controllers/textController.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 from bottle import request
 from services.hocrService import HocrService
-
+import services.libhOCR as lib
 
 class TextController:
     def __init__(self, hocr_service):
@@ -9,12 +10,25 @@ class TextController:
     def get_text(self, id):
         text_name = self.hocr_service.get_path_file(id)
         print(text_name)
-        return self.hocr_service.get_hocr_lines(text_name)
+        temp_line = "a"
+        result_lines = ""
+        number_line = 1
+        while(temp_line!=""):
+            temp_line = lib.get_line(text_name, number_line)
+            number_line+=1
+            result_lines += temp_line+"\n"
+        return {'text' : result_lines}
+
+
 
     def save_text(self, id):
         data = request.json
         print(data)
         path = self.hocr_service.get_path_file(id)
         status = self.hocr_service.save_hocr_text(path, data['text'])
-
-        return {'status': status}
+        lines = data['text'].split("\n")
+        number_lines = 1
+        for x in range(len(lines)):
+            lib.edit_line(path, number_lines, lines[x])
+            number_lines+=1
+        return {'status' : 'status'}
