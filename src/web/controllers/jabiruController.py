69cbc17567ead061024277cb50998292ec53a37a
Jose Paulo Yock Fuentes <jyock1997@gmail.com>
2017-09-27 09:24:20 -0600

    Change the format of the code following the standard pep8 and add documentation in the code

--- src/web/controllers/jabiruController.py
+++ src/web/controllers/jabiruController.py
@@ -1,6 +1,10 @@
 class JabiruController:
+
     def __init__(self, env):
         self.env = env
 
     def home(self):
+        '''
+            Return a Jabiru home page.
+        '''
         return self.env.get_template('jabiru.jade').render()
