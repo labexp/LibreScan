3504bdfe865a7d65115f8eff43c932221fbc199b
5up3rD4n1 <superdaniel1920@hotmail.com>
2018-04-25 19:13:59 -0600

    Removing unfinished ocr editor step

--- src/web/assets/js/scan.js
+++ src/web/assets/js/scan.js
@@ -1,23 +1,22 @@
+$(document).ready(function () {
 
-$(document).ready(function(){
 
-
-   $('body').on('click', '.photo' ,function(){
-       var name = $(this).attr('name');
-       if (!name) return;
-        var src = '/photo/'+name;
+    $('body').on('click', '.photo', function () {
+        var name = $(this).attr('name');
+        if (!name) return;
+        var src = '/photo/' + name;
         var img = '<img src="' + src + '" class="img-responsive"/>';
 
-        $('#photo-modal').on('shown.bs.modal', function(){
+        $('#photo-modal').on('shown.bs.modal', function () {
             $('#photo-modal .modal-body').html(img);
         });
-        $('#photo-modal').on('hidden.bs.modal', function(){
+        $('#photo-modal').on('hidden.bs.modal', function () {
             $('#photo-modal .modal-body').html('');
         });
-       $('#photo-modal').modal();
+        $('#photo-modal').modal();
     });
 
-    $('body').on('click', '.capture-btn' ,function(){
+    $('body').on('click', '.capture-btn', function () {
         if ($('.recapture-btn').hasClass('btn-disabled')) {
             $('.recapture-btn').removeClass('btn-disabled');
         }
@@ -26,23 +25,23 @@ $(document).ready(function(){
         $.ajax({
             method: 'post',
             url: '/photo'
-        }).done(function(response) {
+        }).done(function (response) {
             $("#loading-cams-msg").css("display", "block");
             $("#loading-div").css("display", "none");
-            if(response.status == -1) {
+            if (response.status == -1) {
                 $('#prepare-cams-modal').modal('show');
                 $('#modal-pcm-capture-error').css('display', 'block');
-            }else {
+            } else {
                 console.log('Done cap');
-                $('#photo1').attr( "src", "/thumbnail/" + response.photo1);
-                $('#photo2').attr( "src", "/thumbnail/"+ response.photo2);
-                $('#photo1').attr( "name", response.photo1);
-                $('#photo2').attr( "name", response.photo2);
+                $('#photo1').attr("src", "/thumbnail/" + response.photo1);
+                $('#photo2').attr("src", "/thumbnail/" + response.photo2);
+                $('#photo1').attr("name", response.photo1);
+                $('#photo2').attr("name", response.photo2);
             }
         });
     });
 
-    $('body').on('click', '#check-ocr-btn' ,function(e){
+    $('body').on('click', '#check-ocr-btn', function (e) {
         e.preventDefault();
         $(".loading-text").css("display", "none");
         $("#loading-process-msg").css("display", "block");
@@ -50,22 +49,22 @@ $(document).ready(function(){
         $.ajax({
             method: 'post',
             url: '/scan/halt'
-        }).done(function(response) {
-            if(response.status == -1) {
+        }).done(function (response) {
+            if (response.status === -1) {
                 alert("Error al procesar imagenes");
-            }else {
+            } else {
                 checkProgress(-1);
             }
         });
     });
 
     function checkProgress(itemsLeft) {
-        if(itemsLeft == 0) progressCompleted();
-        setTimeout(function() {
+        if (itemsLeft === 0) progressCompleted();
+        setTimeout(function () {
             $.ajax({
                 method: 'get',
                 url: '/progress'
-            }).done(function(data){
+            }).done(function (data) {
                 itemsLeft = data.itemsLeft;
                 $("#left-items-span").html(itemsLeft);
                 checkProgress(itemsLeft);
@@ -74,14 +73,19 @@ $(document).ready(function(){
     }
 
     function progressCompleted() {
-        $(".loading-text").css("display", "none");
-        $("#loading-cams-msg").css("display", "block");
-        $("#loading-div").css("display", "none");
-        window.location.href = '/ocrs';
+        $.ajax({
+            method: 'post',
+            url: '/output'
+        }).done(function (output) {
+            // $("#show-output").removeClass('disabled');
+            window.location.href = '/outputPreview';
+        }).fail(function () {
+            alert("Algo no salio bien");
+        });
     }
 
-    $('body').on('click', '.recapture-btn' ,function(){
-        if ($(this).hasClass('btn-disabled')){
+    $('body').on('click', '.recapture-btn', function () {
+        if ($(this).hasClass('btn-disabled')) {
             return;
         }
         $("#loading-div").css("display", "block");
@@ -89,32 +93,32 @@ $(document).ready(function(){
         $.ajax({
             method: 'PUT',
             url: '/photo'
-        }).done(function(response) {
+        }).done(function (response) {
             $("#loading-cams-msg").css("display", "block");
             $("#loading-div").css("display", "none");
-            if(response.status == -1) {
+            if (response.status == -1) {
                 $('#prepare-cams-modal').modal('show');
                 $('#modal-pcm-capture-error').css('display', 'block');
-            }else {
+            } else {
                 console.log('Done recap');
-                $('#photo1').attr( "src", "/thumbnail/" + response.photo1);
-                $('#photo2').attr( "src", "/thumbnail/"+ response.photo2);
-                $('#photo1').attr( "name", response.photo1);
-                $('#photo2').attr( "name", response.photo2);
+                $('#photo1').attr("src", "/thumbnail/" + response.photo1);
+                $('#photo2').attr("src", "/thumbnail/" + response.photo2);
+                $('#photo1').attr("name", response.photo1);
+                $('#photo2').attr("name", response.photo2);
             }
         });
-   });
+    });
 
-    $('body').on('click', '#calibrate-btn' ,function(e){
+    $('body').on('click', '#calibrate-btn', function (e) {
         e.preventDefault();
         $.ajax({
             method: 'POST',
             url: '/camera/calibrate'
-        }).done(function(response){
-            if(response.status == -1) {
+        }).done(function (response) {
+            if (response.status == -1) {
                 $('#prepare-cams-modal').modal('show');
                 $('#modal-pcm-capture-error').css('display', 'block');
-            }else{
+            } else {
                 var message = $('#calibrate-success-message').text()
                 generateNotification('success', message, 'topRight', 2000);
             }
@@ -122,11 +126,10 @@ $(document).ready(function(){
 
     });
 
-    $('#calibrate-btn').hover(function(){
+    $('#calibrate-btn').hover(function () {
         var message = $('#calibrate-message').text();
         generateNotification('information', message, 'topCenter', 6000);
     });
-
 
 
 });
